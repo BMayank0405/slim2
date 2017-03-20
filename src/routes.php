d0642262963848b79b8fa4d8c5fa151592e01a81
BMayank0405 <mayankbharthwal1996@gmail.com>
2017-03-21 00:04:39 +0530

    working ajax

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/name/[{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
@@ -9,7 +9,9 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->post('/order/{id}', function ($request, $response, $args) {
+$app->post('/order', function ($request, $response, $args) {
 	$parsedBody = $request->getParsedBody();
-	return $response->withJson($parsedBody);
+	//return $response->withJson($parsedBody);
+    //$json['data']='ok';
+    echo json_encode($parsedBody);
 });
