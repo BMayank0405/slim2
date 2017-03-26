57931499b2dea506008f7bd74cd7bd699180b5a4
BMayank0405 <mayankbharthwal1996@gmail.com>
2017-03-27 00:29:37 +0530

    validating user input

---
+++ src/OrdersToBeInserted.php
@@ -0,0 +1,144 @@
+<?php
+
+$app->post('/placeOrder', function($request,$response,$args){
+  global $db;
+  $client = $request->getParam("client_order_id");
+  $awb_no = $request->getParam('awb_number');
+  $pincode = $request->getParam('pincode');
+  $customer_name = $request->getParam('customer_name');
+  $customer_phone = $request->getParam('customer_phone');
+  $customer_address = $request->getParam('customer_address');
+  $c_city = $request->getParam('c_city');
+  $c_state = $request->getParam('c_state');
+  $declared_value = $request->getParam('declared_value');
+  $cod_amount = $request->getParam('cod_amount');
+  $deliver_type = $request->getParam('deliver_type');
+
+  //here onwards json object start so we use some functions to make them store in mariadb
+  $pickup_address =$request->getParam('pickup_address_attributes');
+  $rto_attributes =$request->getParam('rto_attributes');
+  $rts_attributes =$request->getParam('rts_attributes');
+  $not_delivered = $request->getParam('return_type_if_not_delivered');
+  $skus = input_data($request->getParam('skus_attributes'));
+
+
+//validation of the input data
+  if(empty($client)){
+    $errors[]='client_order needed to be filled';
+  }
+  if(empty($awb_no)){
+    $errors[]='awb_number needed to be filled';
+  }
+  if(!(preg_match("/^[0-9]{6}$/",$pincode))){
+    $errors[]='pincode of 6 digit needed to be filled';
+  }
+  if(!(preg_match("/^([0-9]){10,}$/",$customer_phone))){
+    $errors[]='customer phone should be 10 digit long';
+  }
+  if(empty($customer_address)){
+    $errors[]='customer_address needed to be filled';
+  }
+  if(empty($c_city)){
+    $errors[]='c_city needed to be filled';
+  }
+  if(empty($c_state)){
+    $errors[]='c_state needed to be filled';
+  }
+  if(empty($declared_value)){
+    $errors[]='declared_value needed to be filled';
+  }
+  if(empty($cod_amount)){
+    $errors[]='cod_amount needed to be filled';
+  }
+  if(empty($deliver_type)){
+    $errors[]='deliver_type needed to be filled';
+  }
+  if(!(preg_match("/^[0-9]{6}$/",$pickup_address['pincode']))){
+    $errors[]='pincode of 6 digit needed to be filled in pickup address';
+  }
+  if(array_search('',$rto_attributes)===false){
+    if(!(preg_match("/^[0-9]{6}$/",$rto_attributes['pincode']))){
+      $errors[]='pincode of 6 digit needed to be filled in rto address';
+    }
+    if(!(preg_match("/^([0-9]){10,}$/",$rto_attributes['contact_no']))){
+      $errors[]='contact number in rto address should be 10 digit long';
+    }
+  }else {
+    $errors[] = 'all the fields of rto_attributes are mandatory';
+  }
+
+  if(array_search('',$rts_attributes)===false){
+    if(!(preg_match("/^[0-9]{6}$/",$rts_attributes['pincode']))){
+      $errors[]='pincode of 6 digit needed to be filled in rts address';
+    }
+    if(!(preg_match("/^([0-9]){10,}$/",$rts_attributes['contact_no']))){
+      $errors[]='contact number in rts address should be 10 digit long';
+    }
+  }else {
+    $errors[] = 'all the fields of rts_attributes are mandatory';
+  }
+//end of validation of input data
+
+
+try {
+
+  if(empty($errors)){
+      $pickup_address = pickup($pickup_address);
+      $rto_attribute = json_object($rto_attribute);
+      $rts_attribute = json_object($rts_attribute);
+
+      $query ="INSERT INTO items
+        VALUES (NULL,
+        '$client','$awb_no',$pincode,'$customer_name','$customer_phone','$customer_address','$c_city','$c_state',
+        $declared_value,$cod_amount,'$deliver_type',$pickup_address,$rto_attributes,$rts_attributes,'$not_delivered',
+        COLUMN_CREATE($skus));";
+
+      $result = $db->query($query);
+      var_dump($result);
+      try{
+        if($result){
+              $r_query = "SELECT id,client_order_id,  awb_number,	pincode,	customer_name,
+                        customer_phone,customer_address,c_city,c_state,declared_value,
+                        cod_amount,deliver_type,
+                        column_json(pickup_address_attributes) as pickup,
+                        column_json(rto_attributes) as rto,
+                        column_json(rts_attributes) as rts,
+                        return_type_if_not_delivered,
+                        column_json(skus_attributes) as skus
+                         FROM `items` ";
+            $r_result = $db->query($r_query);
+            if($r_result){
+            while($r_row = $r_result->fetch_assoc()){
+              echo '{'."\r\n";
+              echo '"message":"Success",'."\r\n";
+              echo '"Client reqiest:"{'."\r\n";
+              maybe_json_encode($r_row);
+              echo '}';
+              echo "\r\n";
+
+            }
+          }
+          else {
+          echo '{"error": "some error may have occured"}';
+          }
+        }
+      else{
+        throw new Exception('there might some problem with your entries')
+      }
+    }
+    catch (Exception $ex){
+      $response->write($ex->getMessage());
+      return $response;
+    }
+  }
+  else {
+    throw new Exception(json_encode($errors,JSON_PRETTY_PRINT));
+  }
+}
+catch(Exception $ex) {
+  $response->write($ex->getMessage());
+  return $response;
+}
+});
+
+?>
--- src/routes.php
+++ src/routes.php
@@ -5,8 +5,8 @@ use Lcobucci\JWT\Builder;
 use Lcobucci\JWT\ValidationData;
 use Lcobucci\JWT\Parser;
 
-require_once __DIR__ . '/../config/config.php';
 require_once __DIR__.'/../src/functions.php';
+require_once __DIR__ . '/../config/config.php';
 
 $app->post('/getauthtoken', function ($request, $response, $args) {
   $parsedBody = $request->getParsedBody();
@@ -48,21 +48,7 @@ $app->get('/testtoken', function ($request, $response, $args) {
 
 
 //route for getting detail of a single awb number
-$app->get('/awb/{awb}',function(Request $request,Response $response,$args){
-  global $db;
-  $awb = $args['awb'];
-  $query = "SELECT * FROM cutomers";
-  $result = $db->query($query);
-  if($result){
-    while($row = $result->fetch_assoc()){
-      echo json_encode($row,JSON_PRETTY_PRINT);
-      echo "\r\n";
-    }
-  }
-  else {
-    echo '{"message": "Invalid AWB Number"}';
-  }
-});
+
 
 //route for getting details of multiple awb numbers
 $app->post('/order', function ($request, $response, $args) {
@@ -105,77 +91,18 @@ $app->post('/order', function ($request, $response, $args) {
 //});
 
 $app->post('/pl', function ($request, $response, $args) {
-  $data = $request->getParam('rts_attributes'); //parsedBody is an array of objects
-  print_r($data);
-  echo json_encode($data);
+  $data = $request->getParam('client'); //parsedBody is an array of objects
+if (empty($client)){
+  echo 'success';
+}
+else{
+  echo 'fail';
+}
 });
 
 
-
-$app->post('/placeOrder', function($request,$response,$args){
-  global $db;
-  $client = $request->getParam("client_order_id");
-  $awb_no = $request->getParam('awb_number');
-  $pincode= $request->getParam('pincode');
-  $customer_name = $request->getParam('customer_name');
-  $customer_phone = $request->getParam('customer_phone');
-  $customer_address = $request->getParam('customer_address');
-  $c_city = $request->getParam('c_city');
-  $c_state = $request->getParam('c_state');
-  $declared_value = $request->getParam('declared_value');
-  $cod_amount = $request->getParam('cod_amount');
-  $deliver_type = $request->getParam('deliver_type');
-
-  //here onwards json object start so we use some functions to make them store in mariadb
-  $pickup_address = pickup($request->getParam('pickup_address_attributes'));
-  $rto_address =json_object($request->getParam('rto_attributes'));
-  $rts_address =json_object($request->getParam('rts_attributes'));
-  $not_delivered = $request->getParam('return_type_if_not_delivered');
-  $skus = input_data($request->getParam('skus_attributes'));
-
-  $query ="INSERT INTO items
-    VALUES (NULL,
-    '$client','$awb_no',$pincode,'$customer_name','$customer_phone','$customer_address','$c_city','$c_state',
-    $declared_value,$cod_amount,'$deliver_type',$pickup_address,$rto_address,$rts_address,'$not_delivered',
-    COLUMN_CREATE($skus));";
-
-  $result = $db->query($query);
-  var_dump($result);
-  if($result){
-          $r_query = "SELECT id,client_order_id,  awb_number,	pincode,	customer_name,
-                    customer_phone,customer_address,c_city,c_state,declared_value,
-                    cod_amount,deliver_type,
-                    column_json(pickup_address_attributes) as pickup,
-                    column_json(rto_attributes) as rto,
-                    column_json(rts_attributes) as rts,
-                    return_type_if_not_delivered,
-                    column_json(skus_attributes) as skus
-                     FROM `items` ";
-        $r_result = $db->query($r_query);
-        if($r_result){
-        while($r_row = $r_result->fetch_assoc()){
-          echo '{'."\r\n";
-          echo '"message":"Success",'."\r\n";
-          echo '"Client reqiest:"{'."\r\n";
-          maybe_json_encode($r_row);
-          echo '}';
-          echo "\r\n";
-
-        }
-      }
-        else {
-        echo '{"error": "some error may have occured"}';
-      }
-  }
-  else {
-
-  }
-
-});
-
-
-
-
+// for inserting new order in the database
+require_once __DIR__ . '/OrdersToBeInserted.php';
 
 
 
